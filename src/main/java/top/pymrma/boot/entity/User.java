e4b3a42881de2904cd90e1f26f157935480802b1
pymrm <pymrma@163.com>
2026-07-15 14:36:09 +0800

    controller

---
+++ src/main/java/top/pymrma/boot/entity/User.java
@@ -0,0 +1,29 @@
+package top.pymrma.boot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String userName;
+    private String avatarUrl;
+    @Column(nullable = false)
+    private String email;
+    private String bio;
+    private String hobbies;
+    private String preferences;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime createAt;
+}
