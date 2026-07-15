b82b5e5f96b718fbcce73444e207337c2b8aa42c
pymrm <pymrma@163.com>
2026-07-15 09:27:27 +0800

    first commit

---
+++ src/test/java/top/pymrma/boot/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package top.pymrma.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
