b153647c716a06343d57d5260ce6237900086e73
pymrm <pymrma@163.com>
2026-07-21 18:03:25 +0800

    2026-07-21

--- src/main/java/top/pymrma/boot/services/FileService.java
+++ src/main/java/top/pymrma/boot/services/FileService.java
@@ -1,11 +1,14 @@
 package top.pymrma.boot.services;
 
 import org.springframework.web.multipart.MultipartFile;
+import top.pymrma.boot.entity.File;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface FileService {
+
+    boolean createFile(File file);
 
     List<String> upload(MultipartFile[] files) throws IOException;
 
--- src/main/java/top/pymrma/boot/services/impl/FileServiceImpl.java
+++ src/main/java/top/pymrma/boot/services/impl/FileServiceImpl.java
@@ -4,16 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.PathMatcher;
 import org.springframework.web.multipart.MultipartFile;
+import top.pymrma.boot.entity.File;
+import top.pymrma.boot.repository.FileRepository;
 import top.pymrma.boot.services.FileService;
 import top.pymrma.boot.utils.DateUtil;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,30 +24,46 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
 
-    private final PathMatcher pathMatcher;
+    private final FileRepository fileRepository;
+
     @Value("${file.upload.dir}")
     private String uploadDir;
+    private java.io.File fullPath;
 
-    private File fullPath;
+    @Override
+    public boolean createFile(File file) {
+        File savedFile = fileRepository.save(file);
+        return savedFile.getId() != null;
+    }
 
     // 多文件上传
     @Override
     public List<String> upload(MultipartFile[] files) throws IOException {
-        String destPath = getOrCreateDirectory();
+        List<String> destPathList = new ArrayList<>();
         for (MultipartFile file : files) {
+            String destPath = getOrCreateDirectory();
+
             String fileName = generateFileName(file);
-            file.transferTo(new File(this.fullPath + File.separator + fileName));
+            Path path = Paths.get(fullPath.getPath(), fileName);
+            file.transferTo(path);
+
+            destPathList.add(destPath);
         }
-        return null;
+        log.info("目标文件列表：{}", destPathList);
+        return destPathList;
     }
 
     //单文件上传
     @Override
     public String upload(MultipartFile file) throws IOException {
-        String destPath = getOrCreateDirectory();
+
         String fileName = generateFileName(file);
-        file.transferTo(new File(this.fullPath + File.separator + fileName));
-        return destPath + fileName;
+        String destPath = getOrCreateDirectory() + fileName;
+        log.info("目标文件：{}", destPath);
+        Path path = Paths.get(fullPath.getPath(), fileName);
+        file.transferTo(path);
+
+        return destPath;
     }
 
     //创建目录
@@ -53,12 +71,12 @@ public class FileServiceImpl implements FileService {
         String year = String.valueOf(LocalDate.now().getYear());
         String month = String.valueOf(LocalDate.now().getMonth()).toLowerCase();
 
-        if (uploadDir.endsWith(File.separator)) {
-            uploadDir = uploadDir + File.separator;
+        if (!uploadDir.endsWith(java.io.File.separator)) {
+            uploadDir = uploadDir + java.io.File.separator;
         }
 
-        String destPath = year + File.separator + month + File.separator;
-        this.fullPath = new File(uploadDir + destPath);
+        String destPath = Paths.get(year, month) + java.io.File.separator;
+        fullPath = new java.io.File(uploadDir + destPath);
         if (!this.fullPath.exists()) {
             boolean created = this.fullPath.mkdirs();
             if (!created) {
@@ -73,7 +91,10 @@ public class FileServiceImpl implements FileService {
     public String generateFileName(MultipartFile file) {
         String currentDate = DateUtil.formatDate(new Date(System.currentTimeMillis()), "yyyyMMdd");
         String originalFilename = file.getOriginalFilename();
-        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String extension = null;
+        if (originalFilename != null) {
+            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        }
         UUID uuid = UUID.randomUUID();
         String fileName = uuid + "-" + currentDate + extension;
 
